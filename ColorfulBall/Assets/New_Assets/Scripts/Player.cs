40dee893292ab2d85cdd456fedcaf7600bb89cba
SaeMatayoshi <c18016@std.it-college.ac.jp>
2019-07-31 14:32:21 +0900

    アスペクト比の修正 Playerスクリプトを修正

---
+++ ColorfulBall/Assets/New_Assets/Scripts/Player.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    // レーン幅の上限
+    const int minLane = -1;
+    const int maxLane = 1;
+    // レーン移動時の幅
+    const float laneWidth = 2.0f;
+
+    CharacterController CharaCon;
+    Vector3 ballMove = Vector3.zero;
+
+    // 今ボールがいるレーン
+    int targetLane;
+
+    // ボールの重力
+    float gravity = 20;
+
+    float accelerationZ = 20;
+    float speedZ = 15;
+    float speedX = 20;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        CharaCon = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        PlayerGetKeyDown();
+
+        BallMove();
+	}
+
+    // ボールの動き
+    void BallMove()
+    {
+        ballMove.z = Mathf.Clamp(accelerationZ, 0, speedZ);
+
+        float ratioX = (targetLane * laneWidth - transform.position.x) / laneWidth;
+        ballMove.x = ratioX * speedX;
+
+        ballMove.y -= gravity + Time.deltaTime;
+
+        Vector3 globalDirection = transform.TransformDirection(ballMove);
+        CharaCon.Move(globalDirection * Time.deltaTime);
+    }
+
+    // プレイヤーがキーを押したときの処理
+    void PlayerGetKeyDown()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+            if (targetLane > minLane)
+                targetLane--;
+
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+            if (targetLane < maxLane)
+                targetLane++;
+    }
+
+
+}
---
+++ ColorfulBall/Assets/New_Assets/Scripts/SceneController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    // タイトルシーンに遷移
+    public void LoadScene_Title()
+    {
+        SceneManager.LoadScene("Title");
+    }
+
+    // メインシーンに遷移
+	public void LoadScene_Main ()
+	{
+		SceneManager.LoadScene ("Main");
+	}
+
+    // 赤決定シーンに遷移
+	public void LoadScene_Decision_red ()
+	{
+		SceneManager.LoadScene ("Decision_red");
+	}
+
+    // 青決定シーンに遷移
+	public void LoadScene_Decision_blue(){
+		SceneManager.LoadScene ("Decision_blue");
+	}
+
+    // 緑決定シーンに遷移
+	public void LoadScene_Decision_green(){
+		SceneManager.LoadScene ("Decision_green");
+	}
+
+    // 赤プレイシーンに遷移
+	public void LoadScene_PlayRed ()
+	{
+		SceneManager.LoadScene ("PlayRed");
+	}
+
+    // 青プレイシーンに遷移
+	public void LoadScene_PlayBlue(){
+		SceneManager.LoadScene ("PlayBlue");
+	}
+
+    // 緑プレイシーンに遷移
+	public void LoadScene_PlayGreen(){
+		SceneManager.LoadScene ("PlayGreen");
+	}
+
+    // リザルトシーンに遷移
+	public void LoadScene_Result(){
+		SceneManager.LoadScene ("Result");
+	}
+}
